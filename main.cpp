d31e4644bfa916546c7cebf514668ecb938a0c8c
Squarish_Star <abhishekhede4@gmail.com>
2025-10-01 16:45:39 +0530

    Add initial documentation for Rubik's Cube representation and solving methods
    - Documented the structure for representing a Rubik's Cube using 3D and 1D arrays.
    - Outlined basic operations and the need for an abstract class for solver implementations.
    - Discussed methods for generating a shuffled Rubik's Cube and the implementation of random shuffle.
    - Provided guidelines for header and CPP file structure, including enum usage for colors.
    - Included compilation instructions for building the project.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,68 @@
-#include <bits/stdc++.h>
+#include "Model/RubiksCube3dArray.cpp"
+#include "Model/RubiksCube1dArray.cpp"
+#include "Model/RubiksCubeBitboard.cpp"
+// #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+
+//   RubiksCube3dArray cube;
+//   cube.print();
+//   cube.l();
+//   cube.print();
+//    vector<RubiksCube::MOVE> shuffle= cube.randomShuffleCube(3);
+//    for(auto move: shuffle){
+//        cout<<RubiksCube::getMove(move)<<" ";
+//    }
+//    cout<<endl;
+//     cube.print();
+
+// cube.u();
+// cube.print();
+// if(cube.isSolved()){
+//     cout<<"Solved"<<endl;
+// }
+// else{
+//     cout<<"Not Solved"<<endl;               
+// }
+// cube.uPrime();
+// if(cube.isSolved()){
+//     cout<<"Solved"<<endl;
+// }
+// else{
+//     cout<<"Not Solved"<<endl;               
+// }
+
+// RubiksCube1dArray cube;
+//   cube.print();
+//   cube.l();     
+//     cube.print();
+//     vector<RubiksCube::MOVE> shuffle= cube.randomShuffleCube(3);
+//     for(auto move: shuffle){
+//         cout<<RubiksCube::getMove(move)<<" ";
+//     }   
+//     cout<<endl;
+//      cube.print();      
+// cube.u();
+
+// cube.print();
+// if(cube.isSolved()){
+//     cout<<"Solved"<<endl;
+// }
+// else{           
+//     cout<<"Not Solved"<<endl;               
+// }
+// cube.uPrime();
+// if(cube.isSolved()){
+//     cout<<"Solved"<<endl;
+// }
+// else{           
+//     cout<<"Not Solved"<<endl;               
+// }   
+
+
+
+
+    return 0;
+
+}
